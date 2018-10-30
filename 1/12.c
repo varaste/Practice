97cb0acd47c12a502a7c109646cd935b2bac8df6
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-10-30 21:27:29 +0330

    Add files via upload

---
+++ 1/12.c
@@ -0,0 +1,17 @@
+//ARYA VARASTE
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	int i,j;
+	for(i=1;i<=8;i++){
+		
+		for(j=1;j<=i;j++){
+			printf("%d",j);
+		}
+		
+		printf("\n");
+	}
+	
+}
