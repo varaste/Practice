67de55ead8e2b1764322c5afe10ea67bc8bb1759
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-06-04 01:22:12 +0430

    Test 2 of jadi python go to class - if & else
    	Test 2 of jadi python go to class - if & else

---
+++ Python/Jadi Go To Class/Test2/ElIf.py
@@ -0,0 +1,94 @@
+my_list=[ 2.3, 'car', 12, 46, 'cat']
+if 12 in my_list:
+    print('hello')
+elif 6 > 4:
+    print('bye')
+else:
+    print('nothing')
+
+
+if 6/2:
+    print('three')
+elif 5 :
+    print('five')
+else:
+    print('zero')
+
+
+if False:
+    print('false')
+elif 2**3 == 8 :
+    print('true')
+else:
+    print('none')
+
+
+my_string="hello"
+if 10 % 5:
+    print('true')
+elif "le" not in my_string :
+    print('false')
+else:
+    print('none')
+
+if 6 < 5:
+    print('one')
+elif 7 == 9:
+    print('two')
+print("three")
+
+
+x = 5
+if 8 % 4:
+    x = x - 1
+elif 3 < 4 / 2:
+    x = x - 2
+elif "t":
+    x = x - 3
+else:
+    x = x - 4
+print (x)
+
+
+x = 0
+if 5 in [1, 2, 3, 4]:
+    x = x + 1
+elif 4 == 2:
+    x = x + 2
+elif 7 > 4:
+    x = x + 3
+else:
+    x = x + 4
+print (x)
+
+
+x = 12
+if "x" in "meow" and 4 > 2+1 :
+    x = x / 2
+elif 6 % 2 != 0 :
+    x = x / 3
+elif 2 in ["cat" , "dog" ]:
+    x = x / 4
+else:
+    x = x + 1
+print (x)
+
+
+x = 100
+if x > 10 :
+    x = x + 10
+if x > 20 :
+    x = x + 50
+print(x)
+
+
+x = 100
+if x > 10 :
+    x = x + 10
+elif x > 20 :
+    x = x + 50
+else:
+    x = x + 70
+print(x)
+
+
