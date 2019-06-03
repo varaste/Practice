8aaf53b6232a30f51e5cf73f8b5a12535c29bf59
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-06-04 01:21:03 +0430

    if elif else

---
+++ Python/Jadi Go To Class/Test2/If Cat Dog.py
@@ -0,0 +1,8 @@
+instring = input("Please Enter a String: ")
+
+if "dog" in instring:
+    print("Dog")
+elif "cat" in instring:
+    print("Cat")
+else:
+    print("None")
---
+++ Python/Jadi Go To Class/Test2/if235.py
@@ -0,0 +1,10 @@
+inNum = int(input("Please Enter a Number: "))
+
+if inNum is 2:
+    print("two")
+elif inNum is 3:
+    print("three")
+elif inNum is 5:
+    print("five")
+else:
+    print("other")
