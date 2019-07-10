9ce84c95f59639527cad529f311541d22608b131
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-07-10 14:13:28 +0430

    Add files via upload

---
+++ JavaScrip/1/functions.js
@@ -0,0 +1,15 @@
+var name;
+function changeText()
+{
+	/*
+	document.getElementById("p1").innerHTML = "Visit <a href='http://www.abdollahi.us'>My Web Site</a>"
+	dgdfg
+	dfgdfg
+	gdfgfd
+	*/
+	
+	var text = "Visit <a href='http://www.abdollahi.us'>My Web Site</a>";
+	//text = 2;
+	document.getElementById("p1").innerHTML = text
+	
+}
