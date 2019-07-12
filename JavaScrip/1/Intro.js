d1c2cf24b6026862a1c81f87499abf1fc3e8e5ef
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-07-13 00:17:35 +0430

    Intro

---
+++ JavaScrip/1/Intro.js
@@ -0,0 +1,13 @@
+var name;
+
+function changeText() {
+	/*	dgdfgdfg
+	gdfgfd
+	*/
+	//sdcascihb
+
+	var text = "Visit <a href='https://github.com/varaste'>My Web Site</a>";
+	//text = 2;
+	document.getElementById("p1").innerHTML = text
+
+}
