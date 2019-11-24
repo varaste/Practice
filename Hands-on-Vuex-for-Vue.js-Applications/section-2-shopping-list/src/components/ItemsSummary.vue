6d7460228370e8c09661dc9757c39230bdcc721e
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-11-24 12:29:56 +0330

    Delete ItemsSummary.vue

--- Hands-on-Vuex-for-Vue.js-Applications/section-2-shopping-list/src/components/ItemsSummary.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h3>Summary</h3>
-
-    <p>Total Quantity: {{ $store.getters.totalQuantity }}</p>
-    <p>Total Price: ${{ $store.getters.totalPrice }}</p>
-  </div>
-</template>
