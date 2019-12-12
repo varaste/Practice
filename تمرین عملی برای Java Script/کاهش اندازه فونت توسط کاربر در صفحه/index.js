0daf3a033a566515efb40b37f837ead3139c60cb
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-12-12 12:51:41 +0330

    Delete index.js

--- تمرین عملی برای Java Script/کاهش اندازه فونت توسط کاربر در صفحه/index.js
+++
@@ -1,8 +0,0 @@
-<head>
-<script type="text/javascript" src="js/textsizer.js"></script>
-</head> 
-
-<body> 
-<span dir="rtl">
-<a href="javascript:ts('body',1)">+ afzayesh</a> | <a href="javascript:ts('body',-1)">+ ˜kahesh</a></span>
-</body>
