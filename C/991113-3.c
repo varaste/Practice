083a14f8943f7f985764e67f59cf4e8f244721ce
Arya Varaste <30382009+varaste@users.noreply.github.com>
2021-02-01 13:59:58 +0330

    Add files via upload

---
+++ C/991113-3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+ 
+main() {
+    
+    int Smallest;
+    int Secondsmallest;
+    int array[100];
+    int size
+    int i;
+    
+    printf("\n How many elements do you want to enter: ");
+    scanf("%d", &size);
+    printf("\nEnter %d elements: ", size);
+    for (int i = 0 ; i < size; i++)
+        scanf("%d", &array[i]);
+    if (array[0] < array[1]) {
+        Smallest = array[0];
+        Secondsmallest = array[1];
+    }
+    else {
+      Smallest = array[1];
+      Secondsmallest = array[0];
+    }
+    for (i = 2; i < size; i++) {
+        if (array[i] < Smallest) {
+        Secondsmallest = Smallest;
+        Smallest = array[i];
+        }
+        else if (array[i] < Secondsmallest) {
+            Secondsmallest = array[i];
+        }
+    }
+    printf(" \nSecond smallest element is %d", Secondsmallest);
+}
