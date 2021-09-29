f0bcddb0bb96cd246ddacb42e163b11689e21954
Arya Varaste <30382009+varaste@users.noreply.github.com>
2021-09-29 19:38:45 +0330

    Update pl.py

--- Python/PL/pl.py
+++ Python/PL/pl.py
@@ -1,5 +1,23 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('arya')
-arya
->>> 
+message = 'Arya Varaste'
+
+print(message)
+print("The message len is:", len(message))
+print("The message first char is:", message[0])
+print(message[2:7])
+print(message[:7])
+print(message[2:])
+print(message.lower())
+print(message.upper())
+print(message.count('ar'))
+print(message.find('ar'))
+print(message.find('sina'))
+
+Smessage = message.replace("Arya", "Sina")
+print(message)
+print(Smessage)
+
+Varastes = message + ", " + Smessage
+print(Varastes)
+
+
+
