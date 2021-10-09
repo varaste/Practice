d880e19f8f4003daa3a25bd289e583b4585b310e
Arya Varaste <30382009+varaste@users.noreply.github.com>
2021-10-09 22:33:24 +0330

    Add files via upload

---
+++ Python/Q-U-588.py
@@ -0,0 +1,24 @@
+'''
+n = input()
+
+int_n = int(n)
+arr_numbers = []
+
+for i in range(int_n):
+    numbers = input()
+    int_numbers = int(numbers)
+    arr_numbers.append(int_numbers)
+
+maxmax = max(arr_numbers)
+print(maxmax)
+'''
+
+n = input()
+int_n = int(n)
+arrn = []
+for i in range(int_n):
+    arrn = input()
+    int_arrn = int(arrn)
+
+maxmax = max(arrn)
+print(maxmax)
