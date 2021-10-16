c639835f95ed128debc1991a822f3a6b881bd65e
Arya Varaste <30382009+varaste@users.noreply.github.com>
2021-10-16 20:03:16 +0330

    Add files via upload

---
+++ Python/PL/Python tkinter Basic-Exercise-1.py
@@ -0,0 +1,11 @@
+'''Python tkinter Basic: Exercise-1 with Solution
+Write a Python GUI program to import Tkinter package and create a window and set its title.
+'''
+
+import tkinter as tk
+# Create instance
+parent = tk.Tk()
+# Add a title
+parent.title("-Welcome to Python tkinter Basic exercises-")
+# Start GUI
+parent.mainloop()
