2c53d7c06a41cd138955ac57c8f99528c02f7cb4
Arya Varaste <30382009+varaste@users.noreply.github.com>
2022-07-16 12:05:15 +0430

    Add files via upload

---
+++ Python/PL/source.py
@@ -0,0 +1,12 @@
+def game(number):
+    input_num = number
+    int_input_num = int(input_num)
+    dah = int_input_num//10
+    yek = int_input_num%10
+    difference = 0
+    if dah > yek:
+        difference = dah - yek
+    else:
+        difference = yek - dah
+    print(difference)
+    
